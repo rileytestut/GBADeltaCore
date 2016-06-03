64dcdfaa6514e8caecf2467c11cb7137650409f6
Riley Testut <riley@rileytestut.com>
2016-06-02 19:48:37 -0500

    Initial Commit

---
+++ GBADeltaCore/GBADeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  GBADeltaCore.h
+//  GBADeltaCore
+//
+//  Created by Riley Testut on 6/2/16.
+//  Copyright © 2016 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GBADeltaCore.
+FOUNDATION_EXPORT double GBADeltaCoreVersionNumber;
+
+//! Project version string for GBADeltaCore.
+FOUNDATION_EXPORT const unsigned char GBADeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GBADeltaCore/PublicHeader.h>
+
+
