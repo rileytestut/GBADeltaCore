7c838701fec1cc5b8dbb79a40195bb7b4dade607
Riley Testut <riley@rileytestut.com>
2016-06-03 11:37:56 -0500

    Adds GBADeltaCore.swift

---
+++ GBADeltaCore/GBADeltaCore.swift
@@ -0,0 +1,11 @@
+//
+//  GBADeltaCore.swift
+//  GBADeltaCore
+//
+//  Created by Riley Testut on 6/3/16.
+//  Copyright © 2016 Riley Testut. All rights reserved.
+//
+
+import Foundation
+
+public let kUTTypeGBAGame: CFStringRef = "com.rileytestut.delta.game.gba"
