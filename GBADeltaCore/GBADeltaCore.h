dbe775fc4f116fb31a1fb9d220553149f3f22c69
Riley Testut <riley@rileytestut.com>
2016-07-08 17:37:27 -0500

    Defines supported CheatType's

--- GBADeltaCore/GBADeltaCore.h
+++ GBADeltaCore/GBADeltaCore.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <DeltaCore/DeltaCore.h>
 
 //! Project version number for GBADeltaCore.
 FOUNDATION_EXPORT double GBADeltaCoreVersionNumber;
@@ -16,3 +17,8 @@ FOUNDATION_EXPORT const unsigned char GBADeltaCoreVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GBADeltaCore/PublicHeader.h>
 #import <GBADeltaCore/GBAEmulatorBridge.h>
+
+// Extensible Enums
+FOUNDATION_EXPORT CheatType const CheatTypeActionReplay;
+FOUNDATION_EXPORT CheatType const CheatTypeGameShark;
+FOUNDATION_EXPORT CheatType const CheatTypeCodeBreaker;
---
+++ GBADeltaCore/GBADeltaCore.m
@@ -0,0 +1,13 @@
+//
+//  GBADeltaCore.m
+//  GBADeltaCore
+//
+//  Created by Riley Testut on 7/8/16.
+//  Copyright © 2016 Riley Testut. All rights reserved.
+//
+
+@import DeltaCore;
+
+CheatType const CheatTypeActionReplay = @"ActionReplay";
+CheatType const CheatTypeGameShark = @"GameShark";
+CheatType const CheatTypeCodeBreaker = @"CodeBreaker";
