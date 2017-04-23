459e779e3d2d1eb352115cb69caada5330b19264
Riley Testut <riley@rileytestut.com>
2017-04-23 02:49:02 -0700

    Refactors logic to match revised DeltaCoreProtocol

--- GBADeltaCore/GBA.swift
+++ GBADeltaCore/GBA.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import AVFoundation
+
+import DeltaCore
 
 public extension GameType
 {
@@ -17,13 +20,28 @@ public struct GBA: DeltaCoreProtocol
 {
     public static let core = GBA()
     
+    public let gameType = GameType.gba
+    
     public let bundleIdentifier: String = "com.rileytestut.GBADeltaCore"
     
-    public let supportedGameTypes: Set<GameType> = [.gba]
+    public let gameSaveFileExtension: String = "sav"
+    
+    public let frameDuration = (1.0 / 60.0)
+    
+    public let supportedRates: ClosedRange<Double> = 1...3
+    
+    public let supportedCheatFormats: [CheatFormat] = {
+        let actionReplayFormat = CheatFormat(name: NSLocalizedString("Action Replay", comment: ""), format: "XXXXXXXX YYYYYYYY", type: .actionReplay)
+        let gameSharkFormat = CheatFormat(name: NSLocalizedString("GameShark", comment: ""), format: "XXXXXXXX YYYYYYYY", type: .gameShark)
+        let codeBreakerFormat = CheatFormat(name: NSLocalizedString("Code Breaker", comment: ""), format: "XXXXXXXX YYYY", type: .codeBreaker)
+        return [actionReplayFormat, gameSharkFormat, codeBreakerFormat]
+    }()
+    
+    public let audioFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: 32768, channels: 2, interleaved: true)
+    
+    public let videoFormat = VideoFormat(pixelFormat: .bgra8, dimensions: CGSize(width: 240, height: 160))
     
     public let emulatorBridge: EmulatorBridging = GBAEmulatorBridge.shared
-    
-    public let emulatorConfiguration: EmulatorConfiguration = GBAEmulatorConfiguration()
     
     public let inputTransformer: InputTransforming = GBAInputTransformer()
     
