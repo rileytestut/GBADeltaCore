750cb7aff2d9f590cbd11f4ec8b91c10de76eb1a
Riley Testut <riley@rileytestut.com>
2021-11-11 12:44:42 -0800

    Changes SwiftPM product to dynamic library

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "GBADeltaCore",
+            type: .dynamic,
             targets: ["GBADeltaCore", "GBABridge", "VBA-M"]
         )
     ],
